26b3fb2b13ffba5ea34a46b5d10ed4275cd8164c
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-08-06 18:54:11 -0400

    first four problems

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,4 +1,4 @@
-import random
+import random, re
 
 # Read in all the words in one go
 with open("input.txt") as f:
@@ -6,8 +6,34 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+def Library(info): 
+    info = re.sub('[^a-zA-Z0-9 \.\?\!]',' ',info)
+    info = info.split()
+    cache = {}
+    for e in range(len(info)-1):
+        if info[e] not in cache:
+            cache[info[e]] = [info[e+1]]
+        else:
+            cache[info[e]].append(info[e+1])
+
+    return cache
 
 
 # TODO: construct 5 random sentences
 # Your code here
+
+def Construction(library, start):
+    Sentence = [start]
+    current = start
+    while current in library:
+        if current[-1:] in list('.?!'):
+            break
+        else:
+            Sentence.append(random.choice(library[current]))
+            current = Sentence[-1]
+    return " ".join(Sentence)
+L = Library(words)
+K = list(L.keys())
+for e in range(5):
+    print(Construction(L,random.choice(K)))
 
